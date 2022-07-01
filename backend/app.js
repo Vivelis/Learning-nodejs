baecd352690218c03756912dc7c946fb6f29e059
Viveli <maceo.jalbert@epitech.eu>
2022-07-01 15:35:12 +0200

    :refactor: clean code architecture

--- backend/app.js
+++ backend/app.js
@@ -6,10 +6,9 @@ const mongoSecret = require('./mongoDB_secret');
 
 const app = express();
 
-mongoose.connect(mongoSecret,
-  { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+mongoose.connect(mongoSecret,{ useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
